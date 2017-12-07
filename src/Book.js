9bf5db53a628b9c2058d9fa4b51cbb43bb04814a
Sebastiaan van Kuyk <sebastiaan.vankuyk@gmail.com>
2017-12-07 18:20:21 +0100

    book bookshelf and listbooks now all sfc's

--- src/Book.js
+++ src/Book.js
@@ -1,41 +1,41 @@
 import React from 'react'
 
-export default class Book extends React.Component {
+const Book = (props) => {
 
-  setThumbnail = (book) => {
+  const setThumbnail = (book) => {
     const cover = book.imageLinks && book.imageLinks.thumbnail ? book.imageLinks.thumbnail : "https://i2.wp.com/theava.com/wp-content/uploads/2014/11/VantreeseNoPic.jpg?resize=128%2C193";
     return cover
   }
 
-  handleChange = (event) => {
-    const {query, book, title, onUpdateShelf, onRefreshSearch} = this.props
+  const handleChange = (event) => {
+    const {query, book, title, onUpdateShelf, onRefreshSearch} = props
     onUpdateShelf(book, event.target.value, title);
     if(title === 'Search Results' || title === 'In Collection') {
       onRefreshSearch(query)
     };
   }
 
-  render() {
-    const { book, shelf, inList} = this.props
-    return (
-      <li key={book.id + book.title} className={inList}>
-        <div className="book">
-          <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.setThumbnail(book)})` }}></div>
-            <div className="book-shelf-changer">
-              <select value={shelf} onChange={this.handleChange}>
-                <option value="move-to" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
+  const { book, shelf, inList} = props
+  return (
+    <li key={book.id + book.title} className={inList}>
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${setThumbnail(book)})` }}></div>
+          <div className="book-shelf-changer">
+            <select value={shelf} onChange={handleChange}>
+              <option value="move-to" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
           </div>
-          <div className="book-title">{book.title}</div>
-          <div className="book-authors">{book.authors ? book.authors.join(', ') : ''}</div>
         </div>
-      </li>
-    )
-  }
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors ? book.authors.join(', ') : ''}</div>
+      </div>
+    </li>
+  )
 }
+
+export default Book
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -4,43 +4,42 @@ import BookShelf from './BookShelf'
 import PropTypes from 'prop-types'
 
 
-export default class ListBooks extends React.Component {
+const ListBooks = (props) => {
 
-  static propTypes = {
-    books: PropTypes.array.isRequired
-  }
+  const { books, onUpdateShelf} = props
+  let currentlyReadingBooks = books.filter((book)=> book.shelf === 'currentlyReading' )
+  let wantToReadBooks = books.filter((book)=> book.shelf === 'wantToRead' )
+  let readBooks = books.filter((book)=> book.shelf === 'read')
 
-  render() {
-
-    const { books, onUpdateShelf} = this.props
-    let currentlyReadingBooks = books.filter((book)=> book.shelf === 'currentlyReading' )
-    let wantToReadBooks = books.filter((book)=> book.shelf === 'wantToRead' )
-    let readBooks = books.filter((book)=> book.shelf === 'read')
-
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            {currentlyReadingBooks.length > 0 && (
-            <BookShelf books={ currentlyReadingBooks } title='Currently Reading' onUpdateShelf={ onUpdateShelf }/>
-            )}
-            {wantToReadBooks.length > 0 && (
-            <BookShelf books={ wantToReadBooks } title='Want To Read' onUpdateShelf={ onUpdateShelf }/>
-            )}
-            {readBooks.length > 0 && (
-            <BookShelf books={ readBooks } title='Read' onUpdateShelf={ onUpdateShelf }/>
-            )}
-          </div>
-        </div>
-        <Link
-          className='open-search'
-          to='/search'
-          >Add a book
-        </Link>
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
       </div>
-      )
-  }
+      <div className="list-books-content">
+        <div>
+          {currentlyReadingBooks.length > 0 && (
+          <BookShelf books={ currentlyReadingBooks } title='Currently Reading' onUpdateShelf={ onUpdateShelf }/>
+          )}
+          {wantToReadBooks.length > 0 && (
+          <BookShelf books={ wantToReadBooks } title='Want To Read' onUpdateShelf={ onUpdateShelf }/>
+          )}
+          {readBooks.length > 0 && (
+          <BookShelf books={ readBooks } title='Read' onUpdateShelf={ onUpdateShelf }/>
+          )}
+        </div>
+      </div>
+      <Link
+        className='open-search'
+        to='/search'
+        >Add a book
+      </Link>
+    </div>
+    )
 }
+
+ListBooks.propTypes = {
+  books: PropTypes.array.isRequired
+}
+
+export default ListBooks
