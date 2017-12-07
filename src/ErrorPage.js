ef58ee0cc4d84ed5859b4ac0238d6cfc3bf8affb
Sebastiaan van Kuyk <sebastiaan.vankuyk@gmail.com>
2017-12-07 17:53:03 +0100

    ErrorPage is now a sfc

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
-export default class ErrorPage extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <h1>Page Not Found</h1>
-        <p>Sorry, but the page you were trying to view does not exist.</p>
-      </div>
-      )
-  }
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <p>Sorry, but the page you were trying to view does not exist.</p>
+    </div>
+    )
 }
+
+export default ErrorPage
